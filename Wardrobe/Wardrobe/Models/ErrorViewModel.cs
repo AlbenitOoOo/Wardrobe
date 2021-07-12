c85235e81163fc35d6c3d9bd143a38b9becbd776
AlbenitOoOo <82273766+AlbenitOoOo@users.noreply.github.com>
2021-07-12 19:59:36 +0200

    Added Login System Form

---
+++ Wardrobe/Wardrobe/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Wardrobe.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
