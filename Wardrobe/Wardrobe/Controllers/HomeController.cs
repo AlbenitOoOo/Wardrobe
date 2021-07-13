7d92ff92374ae061c03a9b1df8676528a633f7c3
AlbenitOoOo <82273766+AlbenitOoOo@users.noreply.github.com>
2021-07-13 17:10:36 +0200

    Tabela Procedura v0.1
    Ale nie podoba mi się lekko

--- Wardrobe/Wardrobe/Controllers/HomeController.cs
+++ Wardrobe/Wardrobe/Controllers/HomeController.cs
@@ -6,21 +6,58 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Wardrobe.Models;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 
 namespace Wardrobe.Controllers
 {
     public class HomeController : Controller
     {
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        SqlConnection con = new SqlConnection();
+        List<LoginModel> loginModels = new List<LoginModel>();
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            con.ConnectionString = Wardrobe.Properties.Resources.ConnectionString;
+            
         }
 
         public IActionResult Index()
         {
-            return View();
+            FatchDate();
+            return View(loginModels);
+        }
+
+        private void FatchDate()
+        {
+            if(loginModels.Count > 0)
+            {
+                loginModels.Clear();
+            }
+            try
+            {
+                con.Open();
+                com.Connection = con;
+                com.CommandText = "SELECT TOP (1000) [Id]      ,[UserName]      ,[NormalizedUserName]      ,[Email]  FROM[Wardrobe].[dbo].[AspNetUsers]";
+                dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    loginModels.Add(new LoginModel()
+                    { Id = dr["Id"].ToString(),
+                        UserName = dr["UserName"].ToString(),
+                        NormalizedUserName = dr["NormalizedUserName"].ToString() ,
+                        Email = dr["Email"].ToString() });
+                }
+                con.Close();
+            }
+            catch(Exception)
+            {
+                throw;
+            }
         }
 
         public IActionResult Privacy()
---
+++ Wardrobe/Wardrobe/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wardrobe.Models
+{
+    public class LoginModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string NormalizedUserName { get; set; }
+        public string Email { get; set; }
+
+
+    }
+}
