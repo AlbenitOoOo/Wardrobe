d667574560196837ae589e4cfdbaaf3dcc542543
AlbenitOoOo <82273766+AlbenitOoOo@users.noreply.github.com>
2022-02-02 21:59:47 +0100

    identity fro

---
+++ Wardrobe++/Wardrobe-main/Wardrobe_/Wardrobe/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Wardrobe.Data;
+
+[assembly: HostingStartup(typeof(Wardrobe.Areas.Identity.IdentityHostingStartup))]
+namespace Wardrobe.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
